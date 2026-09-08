79f84c495648369e5f9829db6b0b63930673b587
corbano118 <corbano118@gmail.com>
2026-09-07 20:14:52 -0400

    Mantener JAVI activo como asistente del sistema

--- app/src/main/java/com/javi/assistant/JaviVoiceInteractionService.kt
+++ app/src/main/java/com/javi/assistant/JaviVoiceInteractionService.kt
@@ -3,5 +3,18 @@ package com.javi.assistant
 import android.service.voice.VoiceInteractionService
 
 class JaviVoiceInteractionService : VoiceInteractionService() {
-    override fun onReady() { super.onReady() }
+
+    override fun onReady() {
+        super.onReady()
+        if (JaviConfig.wakeEnabled(this)) {
+            try {
+                JaviWakeWordService.start(this)
+            } catch (_: Exception) {
+            }
+        }
+    }
+
+    override fun onShutdown() {
+        super.onShutdown()
+    }
 }
